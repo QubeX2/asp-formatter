9bd13b29ee84f2cac0427c4a222046ad15585d63
Mikael Andersson <qubex2@gmail.com>
2020-02-27 14:19:34 +0100

    updated

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { exists } from 'fs';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -18,42 +19,120 @@ export function activate(context: vscode.ExtensionContext) {
             const edit = new vscode.WorkspaceEdit();
             let indent: number = 0, isAsp: boolean = false;
 
-            for (let i = 0; i < 100; i++) {
-                let line = document.lineAt(i);
+            let indenters = [
+                'if',
+                'do while',
+                'select case',
+                'while',
+                'for'
+            ]
 
-                if (line.text.indexOf('<%') != -1) {
+            let outdenters = [
+               'end if',
+               'loop',
+               'end select',
+               'wend',
+               'next'
+            ]
+
+            let skips = [
+                'else', 
+                'elseif'
+            ]
+
+
+
+            for (let i = 0; i < document.lineCount; i++) {
+
+                if(i == 384) {
+                    let n = 0;
+                }
+
+                let line = document.lineAt(i);
+                const n: string = line.text;
+
+                if (n.indexOf('<%') != -1) {
                     isAsp = true;
                 }
 
                 if(isAsp) {
-                    edit.replace(document.uri, line.range, line.text.trim());
+                    let canIndent: boolean = false;
 
-                    if(line.text.toLocaleLowerCase().indexOf('if ') != -1 && line.text.toLocaleLowerCase().indexOf(' then') != -1) {
-                        indent++;
+                    // trim string to begin with
+                    edit.replace(document.uri, line.range, n.trim());
+
+
+                    if(n.match(/^\s*[']|\s*<%\s*[']/gi)) {
+                        if(n.indexOf('%>') != -1) {
+                            isAsp = false;
+                        }
                         continue;
                     }
 
-                    if(line.text.toLocaleLowerCase().indexOf('else') != -1) {
-                        edit.insert(document.uri, line.range.start, ' '.repeat(4 * (indent-1)));
+                    // indenters
+                    for(const indenter of indenters) {
+                        const re = new RegExp(`^\\s*<%\\s+${indenter}|^\\s*${indenter}`, 'gi');
+                        if(n.match(re)) {
+                            if(indenter == 'if') {
+                                if(n.toLocaleLowerCase().indexOf('end if') == -1) {
+                                    if(n.match(/then\s*$|then\s*%>$/gi)) {
+                                        canIndent = true;
+                                    } else {
+                                        // special case comment after eg: Then 'comment
+                                        if(n.match(/then\s*['].*/gi)) {
+                                            canIndent = true;
+                                        } else {
+                                            // special case thingy, fix
+                                        }
+                                    }
+                                }
+                            } else {
+                                canIndent = true;
+                            }
+                            if(canIndent) {
+                                indent++;
+                            }
+                            break;
+                        }
+                    }
+
+                    if(canIndent) {
                         continue;
                     }
 
-                    if(line.text.toLocaleLowerCase().indexOf('end if') != -1) {
-                        indent--;
+                    // skips
+                    let doSkip: boolean = false;
+                    for(const skip of skips) {
+                        const re = new RegExp(`^\\s*<%\\s+${skip}|^\\s*${skip}`, 'gi');
+                        if(n.match(re)) {
+                            edit.insert(document.uri, line.range.start, ' '.repeat(4 * (indent-1)));
+                            doSkip = true;
+                            break;
+                        }
+                    }
+
+                    if(doSkip) {
+                        continue;
+                    }
+
+                    // outdenters
+                    for(const outdenter of outdenters) {
+                        const re = new RegExp(`^\\s*<%\\s+${outdenter}|^\\s*${outdenter}`, 'gi');
+                        if(n.match(re)) {
+                            indent--;
+                            break;
+                        }
                     }
 
                     edit.insert(document.uri, line.range.start, ' '.repeat(4 * indent));
-
+                    vscode.workspace.applyEdit(edit);
                 }
 
-                /*
-                */
-
-                if(line.text.indexOf('%>') != -1) {
+                // check if end asp
+                if(n.indexOf('%>') != -1) {
                     isAsp = false;
                 }
             }
-            vscode.workspace.applyEdit(edit);
         }
 
 
